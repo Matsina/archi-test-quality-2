ec48038f2f27337def55ba0539eead2d2d0e1c0a
Jérémie Fauveau <contact@jeremie-fauveau.fr>
2024-10-02 15:16:19 +0200

    1st commit

---
+++ src/order/domain/use-case/set-shipping-address-order.service.ts
@@ -0,0 +1,22 @@
+import { NotFoundException } from '@nestjs/common';
+import { Order } from 'src/order/domain/entity/order.entity';
+import OrderRepository from 'src/order/infrastructure/order.repository';
+
+export class SetShippingAddressOrderService {
+  constructor(private readonly orderRepository: OrderRepository) {}
+
+  public async execute(
+    orderId: string,
+    customerAddress: string,
+  ): Promise<Order> {
+    const order = await this.orderRepository.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundException('Pas de commande');
+    }
+
+    order.setShippingAddress(customerAddress);
+
+    return this.orderRepository.save(order);
+  }
+}
