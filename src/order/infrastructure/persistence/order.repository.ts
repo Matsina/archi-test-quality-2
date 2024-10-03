2e64b124359e8553b5e04335f2d9960e06b87bee
Jérémie Fauveau <contact@jeremie-fauveau.fr>
2024-10-03 12:08:24 +0200

    Correction path des imports

--- src/order/infrastructure/persistence/order.repository.ts
+++ src/order/infrastructure/persistence/order.repository.ts
@@ -1,7 +1,7 @@
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource, Repository } from 'typeorm';
-import { OrderRepositoryInterface } from 'src/order/domain/port/order.repository.interface';
 import { Order } from 'src/order/domain/entity/order.entity';
+import { OrderRepositoryInterface } from 'src/order/domain/port/persistence/order.repository.interface';
 
 
 export default class OrderRepositoryTypeOrm
