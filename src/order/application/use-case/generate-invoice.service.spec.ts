0d6213beaef4ff77f0af2873382c652ea8947a9d
Jérémie Fauveau <contact@jeremie-fauveau.fr>
2024-10-04 09:09:37 +0200

    Test invoice pdf feature

--- src/order/application/use-case/generate-invoice.service.spec.ts
+++ src/order/application/use-case/generate-invoice.service.spec.ts
@@ -2,6 +2,7 @@ import { OrderRepositoryInterface } from 'src/order/domain/port/persistence/orde
 import { NotFoundException } from '@nestjs/common';
 import { GenerateInvoiceService } from '../use-case/generate-invoice.service';
 import { Order, OrderStatus } from '../../domain/entity/order.entity';
+import { PdfGeneratorServiceInterface } from 'src/order/domain/port/pdf/pdf-generator.service-interface';
 
 class OrderRepositoryFake {
   async save(order) {
@@ -18,24 +19,26 @@ class OrderRepositoryFake {
         { productName: 'item 1', price: 10, quantity: 1 },
       ],
       shippingAddress: 'Shipping Address',
-      invoiceAddress: '',
+      invoiceAddress: 'Invoice Address',
     });
 
-    order.id = id;
-    order.status = OrderStatus.PENDING;
+    // order.id = id;
+    // order.status = OrderStatus.PENDING;
 
     return order;
   }
 }
 class PdfGeneratorServiceFake {
-    
+    async generatePdf(text: string): Promise<Buffer>{
+        return Buffer.from(text)
+    }
 }
 
 const orderRepositoryFake =
   new OrderRepositoryFake() as OrderRepositoryInterface;
 
 const pdfGeneratorServiceFake =
-  new PdfGeneratorServiceFake() as OrderRepositoryInterface;
+  new PdfGeneratorServiceFake() as PdfGeneratorServiceInterface;
 
 describe("L'adresse de livraison ne peut être définie si la commande n'a pas été payée", () => {
   it('should return an error', async () => {
